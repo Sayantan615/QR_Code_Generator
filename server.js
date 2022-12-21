cd7ef9825486a5a80de1bcbb802e40a2efb022bf
sayantan <sayantanchowdhury615@gmail.com>
2022-12-21 16:17:54 +0530

    Upload Done

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const { response } = require("express");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// app.post("/", (req, res) => {
+//   const height = req.body.height;
+//   const width = req.body.width;
+//   const data = req.body.data;
+//   const url =
+//     "https://chart.googleapis.com/chart?cht=qr&chs=" +
+//     height +
+//     "x" +
+//     width +
+//     "&chl=" +
+//     data;
+//   https.get(url, (response) => {
+//     response.on("data", (data) => {
+//       console.log(data);
+//     });
+//   });
+// });
+
+app.listen(process.env.PORT | 3000);
